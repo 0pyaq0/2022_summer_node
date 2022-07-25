aef42d22557e79f31959919428e74cd1c936fe4c
LeeJin Sim <s2011@e-mirim.hs.kr>
2022-07-25 15:12:49 +0900

    📝 :: crypto 모듈 실습

--- ch01/module/module01.js
+++ ch01/module/module01.js
@@ -1,3 +1,10 @@
-const odd = "홀수";
+/*const odd = "홀수";
 const even = "짝수";
-module.exports = {odd, even};
+module.exports = {odd, even};*/
+
+/*exports.odd = "홀수";
+exports.odd = "짝수";*/
+
+var result = require('./module01.js');
+console.log(result.circleArea());
+console.log(result.abs(-10));
