5af03e5dfd756e4176060e8a02af5a9c297df85b
LeeJin Sim <s2011@e-mirim.hs.kr>
2022-07-25 11:09:42 +0900

    📝 :: const / let 차이

--- ch01/app05.js
+++ ch01/app05.js
@@ -7,3 +7,14 @@ if(1){
     const y = 20;
 }
 console.log(y); // 범위를 벗어나 에러
+
+
+var x = 10;
+console.log(x);
+
+const y = 100; // 값 바꾸기 X
+console.log(y);
+
+let z = 1000;
+z = 10; // 값 바꾸기 O
+console.log(z);
