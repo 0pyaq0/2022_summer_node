c95f79d782daaab398de53a7a8b85945c2b63aa0
LeeJin Sim <s2011@e-mirim.hs.kr>
2022-07-26 14:07:59 +0900

    📝 :: 오디오 웹서버

--- ch02/server/imgServer/server.js
+++ ch02/server/imgServer/server.js
@@ -2,8 +2,8 @@ var fs = require('fs');
 var http = require('http');
 
 http.createServer(function (request, response) {
-    fs.readFile('ch02/server/imgServer/media/profile.png', function (error, data) {
-        response.writeHead(200, {'Content-Type' : 'image/jpeg'});
+    fs.readFile('ch02/server/imgServer/media/us.mp3', function (error, data) {
+        response.writeHead(200, {'Content-Type' : 'audio/mp3'});
         response.end(data);
     });
 }).listen(4444, function() {
