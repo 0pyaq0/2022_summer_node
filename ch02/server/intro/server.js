1a2c866b003dfa0e3bd3acdb91d4e9f02036b613
LeeJin Sim <s2011@e-mirim.hs.kr>
2022-07-26 15:18:55 +0900

    📑 :: 자기소개 페이지

---
+++ ch02/server/intro/server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer(function(req, res) {
+    try{
+      fs.readFile('ch02/server/intro/index.html', function(err, data){
+        res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'});
+        res.end(data);
+        });
+    }
+    catch(err){
+        console.log(err);
+        res.writeHead(500, {'Content-Type' : 'text/plain; charset=utf-8'});
+        res.end(err);
+    }
+}).listen(4444, () => {
+    console.log('포트 4444 서버 대기중...');
+});
