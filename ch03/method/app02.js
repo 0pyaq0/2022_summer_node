f2d848e5c019d21f7f9ec9050a6725c48f8e4773
LeeJin Sim <s2011@e-mirim.hs.kr>
2022-07-27 11:11:40 +0900

    📑 :: POST 요청방식

---
+++ ch03/method/app02.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (request, response){
+
+    if(request.method == 'GET') {
+        fs.readFile('test.html', function(error, data) {
+            // GET
+            response.writeHead(200, {'Content-Type' : 'text/html'});
+            response.end(data);
+        });
+    }
+    else if(request.method == 'POST') {
+        // POST
+        request.on('data', function(data){
+            response.writeHead(200, {'Content-Type' : 'text/html'});
+            response.end('<h1>' + data + '</h1>');
+        });
+    }
+}).listen(4444, function() {
+    console.log('4444번 포트에서 대기중...');
+});
